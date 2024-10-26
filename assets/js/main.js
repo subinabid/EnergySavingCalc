a5d8f770a7ccc6d079e1f1d9303cbd12fde48543
Subin Abid <io@sabid.in>
2024-10-26 22:44:28 +0530

    easy calc

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ function showDiv(option) {
     });
 }
 
+
 tariff_kl = {
     zone1: 6.5,
     zone2: 7.5,
@@ -35,43 +36,44 @@ tariff_in = {
 
 wastage_ref = {
     manufacturing: .25,
-    technology: .7,
+    technology: .07,
     healthcare: .12,
-    education: .5,
+    education: .05,
     retail: .2
 }
 
 
-
 function calculate_easy() {
-    var cd = document.getElementById('contractdemand-easy').value
-    var usage = document.getElementById('electricity-easy').value;
+    var cd = parseInt(document.getElementById('contractdemand-easy').value, 10);
+    var maxload = parseInt(document.getElementById('maxload-easy').value, 10);
+    var usage = parseInt(document.getElementById('electricity-easy').value, 10);
     var industry = document.getElementById('industry-easy').value;
     var location = document.getElementById('location-easy').value;
-    console.log(cd, usage, industry, location)
+    console.log(cd, maxload, usage, industry, location)
 
-    if (cd == "" || usage == "" || industry == "" || location == "") {
+    if (cd == "" || maxload == "" | usage == "" || industry == "" || location == "") {
         output = "Please fill all fields";
         document.getElementById('result').style.display = 'block';
         document.getElementById('resultplaceholder').innerHTML = output;
         return;
     }
 
-    if (usage < cd) {
-        var cd_wastage = (cd - usage) * 125;
-        var result_cd = "You are using less than your contracted demand and paying INR " + cd_wastage + " every month. Consider reducig your contracted demand";
+    if (maxload < cd) {
+        var cd_wastage = (cd - maxload) * 125;
+        var result_cd = "You are using less than your contracted demand and paying INR " + cd_wastage + " every month. Consider reducig your contracted demand if this is a regular occurance";
     }
-    else if (usage > cd) {
-        var penalty = (usage - cd) * 500;
+    else if (maxload > cd) {
+        var penalty = (maxload - cd) * 500;
         var result_cd = "You are using more than your contracted demand and paying penalty of INR " + penalty + " every month. Consider increasing your contracted demand if this is a regular occurance";
     }
     else {
         var result_cd = "You are using exactly your contracted demand. Great job!";
     }
     var wastage = "" + (usage * wastage_ref[industry] * tariff_in['zone1']);
-    var result_wastage = "Your wastage is " + wastage + " INR every month. Consider reducing wastage by implementing energy efficient practices";
+    var result_wastage = "Your wastage is estimated at " + wastage + " INR every month based on your industry. Consider reducing wastage by implementing energy efficient practices";
 
     document.getElementById('result').style.display = 'block';
+    document.getElementById('resultplaceholder').innerHTML = ""
     document.getElementById('resultplaceholder').innerHTML = result_cd + "<br>" + result_wastage;
 }
 
